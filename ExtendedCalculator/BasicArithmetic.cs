8a908f8eba8eacd12715398c8ff1e6affd5b2f3c
Hans Hedegaard Byager <hans-byager@live.dk>
2018-05-23 08:36:30 +0200

    Added Basic Arithmetic (Addition, Subtraction, Multiplication and Division)

---
+++ ExtendedCalculator/BasicArithmetic.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExtendedCalculator
+{
+    class BasicArithmetic
+    {
+        public double Addition(double Num1, double Num2)
+        {
+            // Adds two parameters together
+            double Value = Num1 + Num2;
+
+            return Value;
+        }
+
+        public double Subtract(double Num1, double Num2)
+        {
+            double Value = Num1 - Num2;
+
+            return Value;
+        }
+
+        public double Multiply(double Num1, double Num2)
+        {
+            double Value = Num1 * Num2;
+
+            return Value;
+        }
+
+        public double Divide(double Num1, double Num2)
+        {
+            double Value = Num1 / Num2;
+
+            return Value;
+        }
+    }
+}
